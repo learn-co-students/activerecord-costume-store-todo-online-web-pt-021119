252f2dc14632a789bdafa6c4c8f9bf1856a5af9e
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-14 01:22:07 +0000

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,2 @@
-# Create your CostumeStore class here
+class Store < ActiveRecord::Base
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      # t.string :location
+      # t.integer :number_of_costumes
+      # t.integer :number_of_employees
+      # t.boolean :business
+      # t.datetime :opening_time
+      # t.datetime :closing_time
+    end
+  end
+end
