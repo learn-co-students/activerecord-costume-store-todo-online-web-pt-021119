8315038f2d017589a9eebc0a03c8ed9e21d57b44
Malcolm Leake <echelon1089@gmail.com>
2019-05-28 22:53:56 +0000

    Done.

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,16 +1,4 @@
 # Create your HauntedHouse class here
 class HauntedHouse < ActiveRecord::Base
 
-def change 
-  create_table :haunted_houses do |t|
-    t.string :name
-    t.string :location
-    t.string :theme
-    t.price :price
-    t.datetime :opening_date
-    t.datetime :closing_date
-    
-  
-  
-  end
 end
